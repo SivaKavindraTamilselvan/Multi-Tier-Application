d79227c7b7d1341ec7ced303b5ee1193026d5a35
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-08 15:36:52 +0530

    changes made in notification interface based on the delegate

--- NotificationAppBuisnessLayerLibrary/Interfaces/INotificationService.cs
+++ NotificationAppBuisnessLayerLibrary/Interfaces/INotificationService.cs
@@ -3,11 +3,7 @@ namespace NotificationAppBuisnessLayerLibrary.Interfaces;
 
 public interface INotification
 {
-    void Send(string message,User user);
-    private bool CheckValidation(User user)
-    {
-        return user != null;
-    }
+    void Send(string message,User user,string service);
     private void Log(string message,User user)
     {
         Console.WriteLine($"Log: {message} for {user.Name}");
