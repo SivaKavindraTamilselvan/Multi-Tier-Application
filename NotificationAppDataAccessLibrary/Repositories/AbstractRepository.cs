be4cc4ab6ed545173480fefa003b27d52b7f3ef8
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-08 12:20:34 +0530

    Data Acess Repositories are created

---
+++ NotificationAppDataAccessLibrary/Repositories/AbstractRepository.cs
@@ -0,0 +1,48 @@
+using NotificationAppDataAccessLibrary.Interfaces;
+namespace NotificationAppDataAccessLibrary.Repositories;
+
+public abstract class AbstractRepository<K,T> : IRepository<K,T> where T : class where K : notnull
+{
+    protected Dictionary<K,T> items = new();
+    public abstract T Create(T item);
+
+    public T? Get(K key)
+    {
+        if(items.ContainsKey(key))
+        {
+            return items[key];
+        }
+        return null;
+    }
+
+    public List<T>? GetAll()
+    {
+        if(items.Count == 0)
+        {
+            return null;
+        }
+        var list = items.Values.ToList();
+        return list;
+    }
+
+    public T? Update(K key,T item)
+    {
+        if(!items.ContainsKey(key))
+        {
+            return null;
+        }
+        items[key] = item;
+        return item;
+    }
+
+    public T? Delete(K key)
+    {
+        if(items.ContainsKey(key))
+        {
+            var item = items[key];
+            items.Remove(key);
+            return item;
+        }
+        return null;
+    }
+} 
