34660614fc4cd3c50f04ceaa3e68aef7ebc1fc16
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-09 16:22:00 +0530

    Organised the admin get functions

--- NotificationAppPresentationLayer/ApplicationFrontend/Admin/AdminGetFunctions/AdminGetChoices.cs
+++ NotificationAppPresentationLayer/ApplicationFrontend/Admin/AdminGetFunctions/AdminGetChoices.cs
@@ -21,49 +21,22 @@ public partial class AdminGetRole
                 {
                     case 1:
                         {
-                            int userid = inputCheck.UserIdInputs();
-                            var user = userService.GetUserById(userid);
-                            if (user == null)
-                            {
-                                Console.WriteLine("User not found");
-                                break;
-                            }
-                            Console.WriteLine(user);
+                            GetUserById();
                             break;
                         }
                     case 2:
                         {
-                            Console.WriteLine("Enter the Email To Get The User");
-                            //Inputs given and validated - check inputcheck file
-                            string email = inputCheck.EmailInputs();
-                            var user = userService.GetUserByEmail(email);
-                            //display if no user with the email id found
-                            if (user == null)
-                            {
-                                Console.WriteLine($"No User Found With Email Address {email}");
-                                break;
-                            }
-                            Console.WriteLine(user);
+                            GetUserByEmail();
                             break;
                         }
                     case 3:
                         {
-                            Console.WriteLine("Enter the PhoneNumber To Get The User");
-                            //Inputs given and validated - check inputcheck file
-                            string phone = inputCheck.PhoneNumberInputs();
-                            var user = userService.GetUserByPhoneNumber(phone);
-                            //display if no user with the phone number found
-                            if (user == null)
-                            {
-                                Console.WriteLine($"No User Found With Phone Number {phone}");
-                                break;
-                            }
-                            Console.WriteLine(user);
+                           GetUserByPhoneNumber();
                             break;
                         }
                     case 4:
                         {
-                            userService.PrintAllUsers();
+                            GetAll();
                             break;
                         }
                     case 0:
--- NotificationAppPresentationLayer/ApplicationFrontend/Admin/AdminGetFunctions/AdminGetFunctions.cs
+++ NotificationAppPresentationLayer/ApplicationFrontend/Admin/AdminGetFunctions/AdminGetFunctions.cs
@@ -5,5 +5,47 @@ namespace NotificationAppPresentationLayer.Application;
 
 public partial class AdminGetRole
 {
-   
+    public void GetUserById()
+    {
+        int userid = inputCheck.UserIdInputs();
+        var user = userService.GetUserById(userid);
+        if (user == null)
+        {
+            Console.WriteLine("User not found");
+            return;
+        }
+        Console.WriteLine(user);
+    }
+    public void GetUserByEmail()
+    {
+        Console.WriteLine("Enter the Email To Get The User");
+        //Inputs given and validated - check inputcheck file
+        string email = inputCheck.EmailInputs();
+        var user = userService.GetUserByEmail(email);
+        //display if no user with the email id found
+        if (user == null)
+        {
+            Console.WriteLine($"No User Found With Email Address {email}");
+            return;
+        }
+        Console.WriteLine(user);
+    }
+    public void GetUserByPhoneNumber()
+    {
+        Console.WriteLine("Enter the PhoneNumber To Get The User");
+        //Inputs given and validated - check inputcheck file
+        string phone = inputCheck.PhoneNumberInputs();
+        var user = userService.GetUserByPhoneNumber(phone);
+        //display if no user with the phone number found
+        if (user == null)
+        {
+            Console.WriteLine($"No User Found With Phone Number {phone}");
+            return;
+        }
+        Console.WriteLine(user);
+    }
+    public void GetAll()
+    {
+        userService.PrintAllUsers();
+    }
 }
