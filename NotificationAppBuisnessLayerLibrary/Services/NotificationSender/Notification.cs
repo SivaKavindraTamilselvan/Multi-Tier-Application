6de7ca0d5398b6b99c5d5f74366e6e512e7e84af
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-09 19:47:18 +0530

    organised the file based on order

--- NotificationAppBuisnessLayerLibrary/Services/NotificationSender/Notification.cs
+++ NotificationAppBuisnessLayerLibrary/Services/NotificationSender/Notification.cs
@@ -7,21 +7,20 @@ namespace NotificationAppBuisnessLayerLibrary.Services;
 
 public abstract class NotificationSenderService : INotificationSender
 {
+    protected string message = "";
+    protected User user = null!;
+    protected string service = "";
+    protected string status = "";
+    protected DateTime? dateTime;
+    NotificationOperation? notificationOperation;
     private readonly INotificationRepository notificationRepo;
     public NotificationSenderService(INotificationRepository _notificationRepo)
     {
         notificationRepo = _notificationRepo;
     }
-    NotificationOperation? notificationOperation;
-
-    protected string message = "";
-    protected User user = null!;
-    protected string service = "";
-
-    protected string status = "";
-    protected DateTime? dateTime;
 
     public abstract void SendNotification();
+    public abstract void LogNotification();
     public void Send(string message, User user, string service)
     {
         this.message = message;
@@ -56,6 +55,4 @@ public abstract class NotificationSenderService : INotificationSender
 
         notificationRepo.Create(notification);
     }
-
-    public abstract void LogNotification();
 }
