2a1ecafa35ace948ab231d95cd229e729fb1a231
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-09 17:14:33 +0530

    added user not found exception

---
+++ NotificationAppModelLibrary/Exception/UserNotFoundException.cs
@@ -0,0 +1,9 @@
+namespace NotificationAppModelLibrary.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException(string message) : base(message)
+    {
+        
+    }
+}
