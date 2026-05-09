ba6e91aeb805977760c37f79ab671e31f1ebaa83
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-09 17:16:07 +0530

    added notification not found exception

---
+++ NotificationAppModelLibrary/Exception/NotificationNotFoundException.cs
@@ -0,0 +1,8 @@
+namespace NotificationAppModelLibrary.Exceptions;
+public class NotificationNotFoundException : Exception
+{
+    public NotificationNotFoundException(string message) : base(message)
+    {
+        
+    }
+}
